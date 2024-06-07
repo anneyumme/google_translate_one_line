04ec3f2c9a65d61bc6b0a6cbaf2a8ed06e888b6c
Ninh Dong <ninhdongnguyen@gmail.com>
2024-06-07 16:37:49 +0700

    fix: bugs

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 chrome.storage.sync.get('extensionEnabled', (data) => {
     if (data.extensionEnabled === true) {
-        let textAreaInput = document.querySelector('#yDmH0d > c-wiz > div > div.ToWKne > c-wiz > div.OlSOob > c-wiz > div.ccvoYb > div.AxqVh > div.OPPzxe > c-wiz.rm1UF.dHeVVb.UnxENd > span > span > div > textarea');
-
-        // Initial call to convert the current content
+        let textAreaInput = document.querySelector('textarea')
+         // Initial call to convert the current content
         textAreaInput.value = convertToLowerCaseWithoutSpace(textAreaInput.value);
-
         // Add an event listener for the 'input' event to trigger conversion on changes
         textAreaInput.addEventListener('input', () => {
             textAreaInput.value = convertToLowerCaseWithoutSpace(textAreaInput.value);
